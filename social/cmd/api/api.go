d7fab97f2fa7933a6ce1786da5f8f99f0ae86cc4
unknown <redietberhanu2@gmail.com>
2026-01-09 18:35:38 +0300

    done up to Setting up the HTTP server and API

--- social/cmd/api/api.go
+++ social/cmd/api/api.go
@@ -20,8 +20,9 @@ type config struct {
 func (app *application) mount() http.Handler{
 	r := chi.NewRouter()
 
+    r.Use(middleware.Recoverer)
 	r.Use(middleware.Logger)
-
+	
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
 	})
