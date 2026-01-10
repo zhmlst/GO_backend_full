3909112989bdb702048bd7808737d9d6748373b8
unknown <redietberhanu2@gmail.com>
2026-01-10 22:45:27 +0300

    adding a hot relode

--- social/cmd/api/api.go
+++ social/cmd/api/api.go
@@ -20,10 +20,17 @@ type config struct {
 func (app *application) mount() http.Handler{
 	r := chi.NewRouter()
 
-    r.Use(middleware.Recoverer)
-	r.Use(middleware.Logger)
-	
-	r.Route("/v1", func(r chi.Router) {
+      r.Use(middleware.RequestID)
+	  r.Use(middleware.RealIP)
+	  r.Use(middleware.Logger)
+	  r.Use(middleware.Recoverer)
+
+	  // Set a timeout value on the request context (ctx), that will signal
+	  // // through ctx.Done() that the request has timed out and further
+	  // // processing should be stopped.
+	  r.Use(middleware.Timeout(60 * time.Second))
+	  
+	  r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
 	})
 
